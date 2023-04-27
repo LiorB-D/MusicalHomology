4f1dfa2fca63f48e135ade14f11d136e0eb2a9fa
Lior Ben-David <liorbd@Outlook.com>
2023-04-27 17:27:16 -0400

    Commented Index.jl

--- index.jl
+++ index.jl
@@ -8,7 +8,7 @@ using Distances
 
 
 
-function computePD(fileStr)
+function computePH(fileStr)
     
     snd, sampFreq = wavread(fileStr)
 
@@ -16,6 +16,7 @@ function computePD(fileStr)
     
     rawAmp = snd[1440000:2880000,1] # Only consider 30-second of song
 
+    # Frequency of Nth bin - n * sampFreq / window length
 
     W = Int(sampFreq * 0.03)         # Window length
     w = ones(W)       # Rectangular analysis window
@@ -25,19 +26,23 @@ function computePD(fileStr)
     X = stft(rawAmp, w, L)    # Analysis
     s = abs2.(X)         # Compute spectrogram
     println("Fourier Transform complete")
-
+    
+    
     #heatmap(s) # Display spectrogram
-    maxAmp = maximum(s)
+
+    
+    maxAmp = maximum(s) # Compute maximization
 
     rawSoundArray = NTuple{4, Float32}[]
 
     for t=1:length(s[1,:])
         for b=1:length(s[:,1])
-            if s[b,t] / maxAmp > 0.025
+            if s[b,t] / maxAmp > 0.025 # Exclude notes that are too insignificant
                 freq = b * sampFreq / W
-                octaveBand = log(2,freq)
-                k = floor(octaveBand)
-                noteRatio = (freq - 2^k) / (2^(k+1) - 2^k)
+                octaveBand = log(2,freq) #Decimal Octave Band
+                k = floor(octaveBand) # Actual Octave Band
+                noteRatio = (freq - 2^k) / (2^(k+1) - 2^k) # Position in the Octave Band ie the note
+                # Polar Decomposition
                 noteX = cos(2pi * noteRatio)
                 noteY = sin(2pi * noteRatio)
                 normalizedTime = 5 * t / length(s[1,:])
@@ -46,27 +51,31 @@ function computePD(fileStr)
             end
         end
     end
+
     println("Sound Array Assembled")
     
+    # Cleanup variables
     rawAmp = 0
     w = 0
     X = 0
     s = 0
-
+    # Kmedoids requires dissimilarity matrix
     DissMatrix = pairwise(Euclidean(), rawSoundArray)
 
     R = kmedoids(DissMatrix, 250)
-    DissMatrix = 0
+    DissMatrix = 0 # Cleanup
+
     println("Medoids Formed")
-    rawSoundArray = rawSoundArray[R.medoids]
+    rawSoundArray = rawSoundArray[R.medoids] # Reduce the array down to just the medoids
     R = 0
     
 
 
-
-    rips = ripserer(rawSoundArray, dim_max = 2, threshold = 1, verbose=true)
+    # Running Persistence homology 
+    rips = ripserer(rawSoundArray, dim_max = 2, threshold = 1, verbose=true) 
     println("Persistence Calculated")
     println(rips)
+    # Ripserer outputs Inf for homology generators that never die, so we need to fix an upper bound to allow the Wasserstein metric computation
     for d=1:3
         for h=1:length(rips[d])
             if rips[d][h][2] == Inf
@@ -78,31 +87,5 @@ function computePD(fileStr)
     return rips
 end
 
-@time april = computePD("April.wav")
-@time blowin = computePD("blowin.wav")
-@time youMayBe = computePD("YouMayBe.wav")
-@time judas = computePD("Judas.wav")
-@time bleecker = computePD("bleecker.wav")
-println("April to blowin")
-Wasserstein()(april,blowin)
-println("April to You May Be")
-Wasserstein()(april,youMayBe)
-println("April to Judas") 
-Wasserstein()(april, judas) 
-println("April to bleecker") 
-Wasserstein()(april, bleecker) 
-println("blowin to You May Be")
-Wasserstein()(blowin,youMayBe)
-println("blowin to Judas") 
-Wasserstein()(blowin, judas) 
-println("blowin to bleecker") 
-Wasserstein()(blowin, bleecker) 
-println("You May be to Judas") 
-Wasserstein()(youMayBe, judas) 
-println("You May be to bleecker") 
-Wasserstein()(youMayBe, bleecker) 
-println("Judas to bleecker") 
-Wasserstein()(judas, bleecker) 
 
-# Frequency of Nth bin - n * sampFreq / window length
 
